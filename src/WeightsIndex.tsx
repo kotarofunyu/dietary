a476ba817cd43ef4949b3056647d4a83b593c839
kotarofunyu <kotarofunyu@gmail.com>
2021-02-06 16:50:22 +0900

    [FIX] 分割すると却って見辛い

--- src/WeightsIndex.tsx
+++ src/WeightsIndex.tsx
@@ -1,6 +1,22 @@
 import React from 'react'
 import { Weight } from './Weight'
-import { BaseTable } from './BaseTable'
+import { makeStyles } from '@material-ui/core/styles'
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from '@material-ui/core'
+
+const columns = [
+  { id: 'id', label: 'id', minWidth: 170 },
+  { id: 'date', label: 'date', minWidth: 100 },
+  { id: 'weight', label: 'weight', minWidth: 170 },
+  { id: 'comment', label: 'comment', minWidth: 170 },
+]
 
 const weights: Array<Weight> = [
   {
@@ -23,10 +39,49 @@ const weights: Array<Weight> = [
   },
 ]
 
+const useStyles = makeStyles({
+  root: {
+    width: '80%',
+    margin: 'auto',
+  },
+  container: {
+    maxHeight: 440,
+  },
+})
+
 export function WeightsIndex() {
+  const datas = weights
+  const classes = useStyles()
   return (
     <div>
-      <BaseTable weights={weights} />
+      <Paper className={classes.root}>
+        <TableContainer className={classes.container}>
+          <Table stickyHeader aria-label="sticky table">
+            <TableHead>
+              <TableRow>
+                {columns.map((column) => (
+                  <TableCell
+                    key={column.id}
+                    style={{ minWidth: column.minWidth }}
+                  >
+                    {column.label}
+                  </TableCell>
+                ))}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {datas.map((data) => (
+                <TableRow hover key={data.id} role="checkbox">
+                  <TableCell>{data.id}</TableCell>
+                  <TableCell>{data.date}</TableCell>
+                  <TableCell>{data.weight}</TableCell>
+                  <TableCell>{data.comment}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Paper>
     </div>
   )
 }
