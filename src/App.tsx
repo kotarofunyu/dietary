b7f0744ccab45bfc4997bc4fe2b8aa1802231fdb
kotarofunyu <kotarofunyu@gmail.com>
2021-02-06 15:33:28 +0900

    [CREATE] HeaderBar Component ( and import )

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,56 @@
 import React from 'react'
-import logo from './logo.svg'
 import './App.css'
+import { HeaderBar } from './HeaderBar'
 
 function App() {
+  const weights: Array<any> = [
+    {
+      id: 1,
+      date: '2020-01-01',
+      weight: 90,
+      comment: '',
+    },
+    {
+      id: 2,
+      date: '2020-01-02',
+      weight: 91,
+      comment: '',
+    },
+    {
+      id: 3,
+      date: '2020-01-03',
+      weight: 91.2,
+      comment: '',
+    },
+  ]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HeaderBar />
+      <div>
+        {weights.map((item) => {
+          return (
+            <>
+              <dl>
+                <dt>日付</dt>
+                <dd>{item.date}</dd>
+              </dl>
+              <dl>
+                <dt>体重</dt>
+                <dd>{item.weight}</dd>
+              </dl>
+              <dl>
+                <dt>コメント</dt>
+                <dd>{item.comment}</dd>
+              </dl>
+            </>
+          )
+        })}
+        <dl>
+          <dt>体重</dt>
+          <dd></dd>
+        </dl>
+      </div>
     </div>
   )
 }
