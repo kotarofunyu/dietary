8b4fed9053851e7c806c97fd7299f959802a6e43
kotarofunyu <k-funyu@sight-visit.com>
2021-02-09 20:18:23 +0900

    [feat] hooks and bind forms

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,15 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { TextField, Button, Icon } from '@material-ui/core'
 export function Login() {
-  const login = (event: React.MouseEvent<HTMLFormElement>) => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = (event: React.MouseEvent<HTMLFormElement>) => {
     alert('submitted')
     event.preventDefault()
   }
   return (
     <div className="login">
-      <form onSubmit={login}>
-        <TextField label="Email" />
-        <TextField label="Password" type="password" />
+      <form onSubmit={handleSubmit}>
+        <TextField
+          label="Email"
+          type="email"
+          name="email"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+        <TextField
+          label="Password"
+          type="password"
+          name="email"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
         <Button type="submit" variant="contained" endIcon={<Icon>Send</Icon>}>
           LOGIN
         </Button>
