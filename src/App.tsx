4472d93781b6b1c88840e38fabc499ddb8b7f4b9
kotarofunyu <k-funyu@sight-visit.com>
2021-02-11 14:17:57 +0900

    [FEAT] test routing

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ import { HeaderBar } from './HeaderBar'
 import { WeightsIndex } from './WeightsIndex'
 import { Form } from './Form'
 import { Login } from './Login'
-import axios from 'axios'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+
+// import axios from 'axios'
 
 // function getUsers() {
 //   axios
@@ -24,10 +26,34 @@ export function App() {
   return (
     <div className="App">
       <HeaderBar />
-      <WeightsIndex />
-      <Form />
-      <Login />
-      {/* <button onClick={getUsers}>get!</button> */}
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">weights</Link>
+              </li>
+              <li>
+                <Link to="/form">form</Link>
+              </li>
+              <li>
+                <Link to="/login">login</Link>
+              </li>
+            </ul>
+          </nav>
+          <Switch>
+            <Route path="/form">
+              <Form />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/">
+              <WeightsIndex />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   )
 }
