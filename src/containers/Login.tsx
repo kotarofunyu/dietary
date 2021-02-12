85f4d19678b1ebbb6e3186267c5b19a75ee0b50a
kotarofunyu <k-funyu@sight-visit.com>
2021-02-12 22:12:15 +0900

    ログイン画面のUIリッチ

--- src/containers/Login.tsx
+++ src/containers/Login.tsx
@@ -1,5 +1,15 @@
 import React, { useState } from 'react'
-import { TextField, Button, Icon } from '@material-ui/core'
+import {
+  TextField,
+  Button,
+  Avatar,
+  CssBaseline,
+  Paper,
+  Grid,
+  Typography,
+} from '@material-ui/core'
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
+import { makeStyles } from '@material-ui/core/styles'
 import { useDispatch } from 'react-redux'
 import * as LoginActions from '../modules/login'
 
@@ -18,27 +28,93 @@ export default function Login() {
     event.preventDefault()
   }
 
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      height: '100vh',
+    },
+    image: {
+      backgroundImage: 'url(https://source.unsplash.com/random)',
+      backgroundRepeat: 'no-repeat',
+      backgroundColor:
+        theme.palette.type === 'light'
+          ? theme.palette.grey[50]
+          : theme.palette.grey[900],
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+    },
+    paper: {
+      margin: theme.spacing(8, 4),
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+    avatar: {
+      margin: theme.spacing(1),
+      backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+      width: '100%',
+      marginTop: theme.spacing(1),
+    },
+    submit: {
+      margin: theme.spacing(3, 0, 2),
+    },
+  }))
+
+  const classes = useStyles()
+
   return (
     <div className="login">
-      <form onSubmit={handleSubmit}>
-        <TextField
-          label="Email"
-          type="email"
-          name="email"
-          value={email}
-          onChange={(event) => setEmail(event.target.value)}
-        />
-        <TextField
-          label="Password"
-          type="password"
-          name="email"
-          value={password}
-          onChange={(event) => setPassword(event.target.value)}
-        />
-        <Button type="submit" variant="contained" endIcon={<Icon>Send</Icon>}>
-          LOGIN
-        </Button>
-      </form>
+      <Grid container component="main" className={classes.root}>
+        <CssBaseline />
+        <Grid item xs={false} sm={4} md={7} className={classes.image} />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <div className={classes.paper}>
+            <Avatar className={classes.avatar}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+            <form className={classes.form} noValidate onSubmit={handleSubmit}>
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="メールアドレス"
+                autoComplete="email"
+                autoFocus
+                value={email}
+                onChange={(event) => setEmail(event.target.value)}
+              />
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="パスワード"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                onChange={(event) => setPassword(event.target.value)}
+                value={password}
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+                className={classes.submit}
+              >
+                ログイン
+              </Button>
+            </form>
+          </div>
+        </Grid>
+      </Grid>
     </div>
   )
 }
