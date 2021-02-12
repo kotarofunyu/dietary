025ec0724d784071d1677e37ce5567863b52c7bb
kotarofunyu <k-funyu@sight-visit.com>
2021-02-12 17:44:01 +0900

    [FEAT] storeの永続化

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,14 +1,23 @@
 import { combineReducers } from 'redux'
 import { createStore } from 'redux'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
 import login from './login'
 
+// ToDo: 現在はログイン中のユーザーのみ保存しているが、他にもstoreで管理する必要が出てきたらwhitelistを追加する
 const reducers = {
   login,
 }
 
-export const rootReducer = combineReducers(reducers)
+const persistConfig = {
+  key: 'currentUser',
+  storage,
+}
+const rootReducer = combineReducers(reducers)
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = createStore(persistedReducer)
+export const persistor = persistStore(store)
 
 export type RootState = ReturnType<typeof rootReducer>
-
-export const store = createStore(rootReducer)
