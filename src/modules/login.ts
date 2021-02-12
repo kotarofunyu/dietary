80e2d65b51b87dacbe288b422e8b330552e3712a
kotarofunyu <k-funyu@sight-visit.com>
2021-02-12 20:30:13 +0900

    [ADD] Action, Action Creator, Reducer

--- src/modules/login.ts
+++ src/modules/login.ts
@@ -4,6 +4,8 @@ const error = 'エラーが発生しました'
 
 export const LOGIN = 'login'
 
+export const LOGOUT = 'logout'
+
 export async function login(email: string, password: string) {
   try {
     const { data } = await axios.post(
@@ -28,6 +30,26 @@ export async function login(email: string, password: string) {
   }
 }
 
+export async function logout() {
+  try {
+    const { data } = await axios.delete('http://localhost:3200/logout', {
+      withCredentials: true,
+      headers: { 'Custom-Header-Element': 'kochandayo' },
+    })
+
+    return {
+      type: LOGOUT,
+      payload: data,
+    }
+  } catch (e) {
+    return {
+      type: LOGOUT,
+      error: true,
+      payload: error,
+    }
+  }
+}
+
 type currentUser = {
   id: number
   name: string
@@ -46,8 +68,13 @@ export default (state: State = initialState, action): State => {
     return state
   }
 
+  console.log(action.payload)
+
   switch (action.type) {
     case LOGIN: {
+      return { ...state, currentUser: action.payload.user }
+    }
+    case LOGOUT: {
       return { ...state, currentUser: action.payload.user }
     }
     default:
