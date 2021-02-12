2b0d233654abc4f1eb3e3060bf79e039f850a9d3
kotarofunyu <k-funyu@sight-visit.com>
2021-02-12 22:13:28 +0900

    [clean]

--- src/App.tsx
+++ src/App.tsx
@@ -13,26 +13,11 @@ export function App() {
     <div className="App">
       <HeaderBar />
       <Router>
-        <div>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">weights</Link>
-              </li>
-              <li>
-                <Link to="/form">form</Link>
-              </li>
-              <li>
-                <Link to="/login">login</Link>
-              </li>
-            </ul>
-          </nav>
-          <Switch>
-            <NoAuth path="/login" component={Login} />
-            <Auth exact path="/" component={WeightsIndex} />
-            <Auth path="/form" component={Form} />
-          </Switch>
-        </div>
+        <Switch>
+          <NoAuth path="/login" component={Login} />
+          <Auth exact path="/" component={WeightsIndex} />
+          <Auth path="/form" component={Form} />
+        </Switch>
       </Router>
     </div>
   )
