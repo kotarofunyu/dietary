6dbbd9020a158b2bdb6102c11a9e2eec709a68a9
kotarofunyu <k-funyu@sight-visit.com>
2021-02-12 22:12:28 +0900

    ナビゲーション

--- src/components/HeaderBar.tsx
+++ src/components/HeaderBar.tsx
@@ -1,24 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
   AppBar,
   Toolbar,
   Typography,
   Button,
   IconButton,
+  Drawer,
+  Divider,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
 } from '@material-ui/core'
-import { makeStyles } from '@material-ui/core/styles'
-import { Menu } from '@material-ui/icons'
+import { makeStyles, useTheme } from '@material-ui/core/styles'
+import {
+  Menu,
+  ChevronLeft,
+  ChevronRight,
+  Inbox,
+  Mail,
+} from '@material-ui/icons'
 import CurrentUser from 'helpers/CurrentUser'
 import { useDispatch } from 'react-redux'
 import * as LogoutAction from '../modules/login'
 
+const drawerWidth = 240
+
+function ListItemLink(props) {
+  return <ListItem button component="a" {...props} />
+}
+
 export function HeaderBar() {
   const currentUser = CurrentUser()
   const dispatch = useDispatch()
+  const [open, setOpen] = useState(false)
+  const theme = useTheme()
 
   const handleClick = async () => {
     const logoutAction = await LogoutAction.logout()
     dispatch(logoutAction)
+  }
+
+  const handleDrawerOpen = () => {
+    setOpen(true)
+  }
+
+  const handleDrawerClose = () => {
+    setOpen(false)
   }
 
   const useStyles = makeStyles((theme) => ({
@@ -31,34 +59,80 @@ export function HeaderBar() {
     title: {
       flexGrow: 1,
     },
+    drawer: {
+      width: drawerWidth,
+      flexShrink: 0,
+    },
+    drawerPaper: {
+      width: drawerWidth,
+    },
+    drawerHeader: {
+      display: 'flex',
+      alignItems: 'center',
+      padding: theme.spacing(0, 1),
+      // necessary for content to be below app bar
+      ...theme.mixins.toolbar,
+      justifyContent: 'flex-end',
+    },
   }))
 
   const classes = useStyles()
 
   return (
-    <AppBar position="static">
-      <Toolbar>
-        <IconButton
-          className={classes.menuButton}
-          edge="start"
-          color="inherit"
-          aria-label="menu"
-        >
-          <Menu />
-        </IconButton>
-        <Typography className={classes.title} variant="h6">
-          dietary
-        </Typography>
-        {currentUser ? (
-          <Button color="inherit" onClick={handleClick}>
-            ログアウト
-          </Button>
-        ) : (
-          <Button href="/login" color="inherit">
-            ログイン
-          </Button>
-        )}
-      </Toolbar>
-    </AppBar>
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            className={classes.menuButton}
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            onClick={handleDrawerOpen}
+          >
+            <Menu />
+          </IconButton>
+          <Typography className={classes.title} variant="h6">
+            dietary
+          </Typography>
+          {currentUser ? (
+            <Button color="inherit" onClick={handleClick}>
+              ログアウト
+            </Button>
+          ) : (
+            <Button href="/login" color="inherit">
+              ログイン
+            </Button>
+          )}
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        className={classes.drawer}
+        variant="persistent"
+        anchor="left"
+        open={open}
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <div className={classes.drawerHeader}>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'ltr' ? <ChevronLeft /> : <ChevronRight />}
+          </IconButton>
+        </div>
+        <Divider />
+        <List>
+          {['記録'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemLink href="/">
+                <ListItemIcon>
+                  {index % 2 === 0 ? <Inbox /> : <Mail />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemLink>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+    </div>
   )
 }
