4e3765cc8b8a46420500f181336a1554a8713972
kotarofunyu <k-funyu@sight-visit.com>
2021-02-13 22:37:01 +0900

    [FEAT] 記録できるところまで出来た

--- src/containers/Form.tsx
+++ src/containers/Form.tsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { TextField, TextareaAutosize, Button, Icon } from '@material-ui/core'
+import { useDispatch } from 'react-redux'
+import * as DiaryActions from '../modules/diary'
 
-function formatDate(dt: Date): string {
+const formatDate = (dt: Date): string => {
   const y = dt.getFullYear()
   const m = ('00' + (dt.getMonth() + 1)).slice(-2)
   const d = ('00' + dt.getDate()).slice(-2)
@@ -9,18 +11,58 @@ function formatDate(dt: Date): string {
 }
 
 export function Form() {
+  const [weight, setWeight] = useState(0)
+  const [date, setDate] = useState(formatDate(new Date()))
+  const [comment, setComment] = useState('')
+  const dispatch = useDispatch()
+
+  const createDiary = async () => {
+    const createDiaryAction = await DiaryActions.createDiary(
+      weight,
+      date,
+      comment,
+    )
+    dispatch(createDiaryAction)
+  }
+
+  const handleSubmit = (event: React.MouseEvent<HTMLFormElement>) => {
+    createDiary()
+    event.preventDefault()
+  }
+
   return (
     <div className="form">
-      <TextField label="weight" type="number" />
-      <TextField
-        label="date"
-        type="date"
-        defaultValue={formatDate(new Date())}
-      />
-      <TextareaAutosize rowsMin={3} placeholder="comment" />
-      <Button variant="contained" endIcon={<Icon>send</Icon>}>
-        Send
-      </Button>
+      <form onSubmit={handleSubmit}>
+        <div className="row">
+          <TextField
+            label="weight"
+            type="number"
+            value={weight}
+            onChange={(e) => setWeight(Number(e.target.value))}
+          />
+        </div>
+        <div className="row">
+          <TextField
+            label="date"
+            type="date"
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+          />
+        </div>
+        <div className="row">
+          <TextareaAutosize
+            aria-multiline
+            rowsMin={3}
+            placeholder="comment"
+            onChange={(e) => setComment(e.target.value)}
+          />
+        </div>
+        <div className="row">
+          <Button variant="contained" endIcon={<Icon>send</Icon>} type="submit">
+            Send
+          </Button>
+        </div>
+      </form>
     </div>
   )
 }
