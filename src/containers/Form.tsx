7979497ae58ed26af4e17049ba59c224a7109610
kotarofunyu <k-funyu@sight-visit.com>
2021-02-21 22:22:00 +0900

    [try] sample

--- src/containers/Form.tsx
+++ src/containers/Form.tsx
@@ -7,6 +7,12 @@ import {
   CircularProgress,
   Select,
   MenuItem,
+  makeStyles,
+  createStyles,
+  useTheme,
+  Input,
+  Chip,
+  Theme,
 } from '@material-ui/core'
 import { useDispatch, useSelector } from 'react-redux'
 import * as DiaryActions from '../modules/diary'
@@ -56,6 +62,67 @@ export function Form({ setOpen: setOpen, data: data }) {
     setProgress(false)
   }
 
+  const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+      formControl: {
+        margin: theme.spacing(1),
+        minWidth: 120,
+        maxWidth: 300,
+      },
+      chips: {
+        display: 'flex',
+        flexWrap: 'wrap',
+      },
+      chip: {
+        margin: 2,
+      },
+      noLabel: {
+        marginTop: theme.spacing(3),
+      },
+    }),
+  )
+
+  const ITEM_HEIGHT = 48
+  const ITEM_PADDING_TOP = 8
+  const MenuProps = {
+    PaperProps: {
+      style: {
+        maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+        width: 250,
+      },
+    },
+  }
+
+  const names = [
+    'Oliver Hansen',
+    'Van Henry',
+    'April Tucker',
+    'Ralph Hubbard',
+    'Omar Alexander',
+    'Carlos Abbott',
+    'Miriam Wagner',
+    'Bradley Wilkerson',
+    'Virginia Andrews',
+    'Kelly Snyder',
+  ]
+
+  function getStyles(name: string, personName: string[], theme: Theme) {
+    return {
+      fontWeight:
+        personName.indexOf(name) === -1
+          ? theme.typography.fontWeightRegular
+          : theme.typography.fontWeightMedium,
+    }
+  }
+
+  const classes = useStyles()
+  const theme = useTheme()
+  const [personName, setPersonName] = React.useState<string[]>([])
+
+  const handleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+    setPersonName(event.target.value as string[])
+  }
+
   return (
     <div className="form">
       <form onSubmit={handleSubmit}>
@@ -87,6 +154,34 @@ export function Form({ setOpen: setOpen, data: data }) {
               )
             })}
           </select>
+        </div>
+        <div className="row">
+          <Select
+            labelId="demo-mutiple-chip-label"
+            id="demo-mutiple-chip"
+            multiple
+            value={personName}
+            onChange={handleChange}
+            input={<Input id="select-multiple-chip" />}
+            renderValue={(selected) => (
+              <div className={classes.chips}>
+                {(selected as string[]).map((value) => (
+                  <Chip key={value} label={value} className={classes.chip} />
+                ))}
+              </div>
+            )}
+            MenuProps={MenuProps}
+          >
+            {names.map((name) => (
+              <MenuItem
+                key={name}
+                value={name}
+                style={getStyles(name, personName, theme)}
+              >
+                {name}
+              </MenuItem>
+            ))}
+          </Select>
         </div>
         <div className="row">
           <TextareaAutosize
